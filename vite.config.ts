ce6e9b86b6b64e59115d988391c17f028e790067
Mohammed haji <mohammedhaji@Mohammeds-MacBook-Pro.local>
2025-07-06 13:06:31 +0100

    add PWA feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt'],
+      manifest: {
+        name: 'ENSET Navigator',
+        short_name: 'Navigator',
+        description: 'Navigation intelligente dans l’ENSET',
+        theme_color: '#002147', // ENSET bleu marine
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
